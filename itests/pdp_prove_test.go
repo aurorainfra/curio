e88bcc87adca45319e7b8fe00f4bd1882c57255c
LexLuthr <88259624+LexLuthr@users.noreply.github.com>
2026-03-05 14:50:02 +0530

    allow piece cid v1 and v2 interoperability (#1048)
    * allow piece cid v1 and v2 interoperability
    
    * fmt
    
    * add retrieval itest
    
    * fix dynamic config for address
    
    * use lists

--- itests/pdp_prove_test.go
+++ itests/pdp_prove_test.go
@@ -14,6 +14,7 @@ import (
 	"github.com/filecoin-project/go-state-types/abi"
 
 	"github.com/filecoin-project/curio/deps/config"
+	"github.com/filecoin-project/curio/itests/helpers"
 	"github.com/filecoin-project/curio/lib/proof"
 	"github.com/filecoin-project/curio/lib/savecache"
 	"github.com/filecoin-project/curio/lib/testutils"
